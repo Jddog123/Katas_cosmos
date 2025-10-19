5e3ad6cbe99b7d755708a60844b9706326c3ebd4
Daniel Urueña Diaz <daniel.uruena@sinco.com.co>
2025-10-19 14:46:45 -0500

    🔵 Si_EnPrimerFrameRealizaStrikeYTercerYCuartoRollTresPuntos_Debe_PuntajeSerVeinteYDos

--- KataCosmos/BowlingScore.Test/IniciarJuego.cs
+++ KataCosmos/BowlingScore.Test/IniciarJuego.cs
@@ -2,6 +2,8 @@
 
 public class IniciarJuego
 {
+    private const int _CantidadFramesMaximo = 10;
+    private const int _CantidadPinesMaximo = 10;
     private readonly List<int> puntajeRolls = new();
     public int ObtenerPuntaje()
     {
@@ -9,10 +11,10 @@ public class IniciarJuego
         int roll = 0;
 
         //Se recorre los 10 frames
-        for (int frame = 1; frame <= 10; frame++)
+        for (int frame = 1; frame <= _CantidadFramesMaximo; frame++)
         {
             //En el primer roll fue strike se suma los puntos de los proximos 2 lanzamientos
-            if (roll == 0 && puntajeRolls[roll] == 10)
+            if (roll == 0 && EsStrike(roll))
             {
                 puntaje += puntajeRolls[roll] + puntajeRolls[roll + 1] + puntajeRolls[roll + 2];
                 //Se suma 1 posiciones en el roll actual ya, para que en strike no se realiza el 2 lanzamiento del frame
@@ -21,7 +23,7 @@ public class IniciarJuego
             //Si es spare se suma los 10 puntos mas el puntaje del siguinere roll lanzado
             else if (EsSpare(roll))
             {
-                //Se obtiene puntaje del frame
+                //Se obtiene puntaje del frame primero (no se quema 10 puntos por cambian la cantidad de pinos por roll)
                 int puntajeFrame = puntajeRolls[roll] + puntajeRolls[roll + 1];
                 puntaje += puntajeFrame + puntajeRolls[roll + 2];
                 //Se suma 2 posiciones en el roll actual ya, para que procese los siguientes roll del siguinete frame del ciclo
@@ -45,6 +47,11 @@ public class IniciarJuego
     
     private bool EsSpare(int rollIndex)
     {
-        return puntajeRolls[rollIndex] + puntajeRolls[rollIndex + 1] == 10;
+        return puntajeRolls[rollIndex] + puntajeRolls[rollIndex + 1] == _CantidadPinesMaximo;
+    }
+    
+    private bool EsStrike(int rollIndex)
+    {
+        return puntajeRolls[rollIndex] == _CantidadPinesMaximo;
     }
 }
