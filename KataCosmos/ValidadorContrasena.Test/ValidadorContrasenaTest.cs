dfd28994ed34f455df9e0605c98951ad81ec3e55
Jddog123 <daniel.uruena@sinco.com.co>
2025-10-26 11:10:42 -0500

    🔴Si_ContrasenaNoTieneOchoCaracteres_Debe_RetornarFalse

--- KataCosmos/ValidadorContrasena.Test/ValidadorContrasenaTest.cs
+++ KataCosmos/ValidadorContrasena.Test/ValidadorContrasenaTest.cs
@@ -1,11 +1,23 @@
+using FluentAssertions;
+
 namespace ValidadorContrasena
 {
     public class ValidadorContrasenaTest
     {
         [Fact]
-        public void Test1()
+        public void Si_ContrasenaNoTieneOchoCaracteres_Debe_RetornarFalse()
         {
+            //Arrange
+            string Contrasena = "Daniel123";
+            //Act
+            bool Resultado = ValidadorContrasena(Contrasena);
+            //Assert
+            Resultado.Should().BeTrue();
+        }
 
+        private bool ValidadorContrasena(string contrasena)
+        {
+            throw new NotImplementedException();
         }
     }
 }
