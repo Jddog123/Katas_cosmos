a092742f14336f0b20498dbc8145e6d57369f0f1
Jddog123 <daniel.uruena@sinco.com.co>
2025-10-27 11:02:52 -0500

    🔴Si_ContrasenaNoTieneAlmenosUnaLetraMayuscula_Debe_RetornarMensajeCausa

--- KataCosmos/ValidadorContrasena.Test/ValidadorContrasenaTest.cs
+++ KataCosmos/ValidadorContrasena.Test/ValidadorContrasenaTest.cs
@@ -132,6 +132,19 @@ namespace ValidadorContrasenaTest
             Resultado.Errores.Should().NotBeEmpty();
             Resultado.Errores.Should().Contain($"Debe tener mas de {minimoCaracteres} caracteres");
         }
-        
+
+        [Fact]
+        public void Si_ContrasenaNoTieneAlmenosUnaLetraMayuscula_Debe_RetornarMensajeCausa()
+        {
+            //Arrange
+            string Contrasena = "daniel123";
+            ContrasenaValidador validator = ContrasenaValidadorFactory.CrearFactory(TipoValidacion.Primera);
+            //Act
+            ContrasenaValidadorResultado Resultado = validator.EsValida(Contrasena);
+            //Assert
+            Resultado.Errores.Should().NotBeNull();
+            Resultado.Errores.Should().NotBeEmpty();
+            Resultado.Errores.Should().Contain($"Debe tener al menos una letra mayuscula");
+        }
     }
 }
