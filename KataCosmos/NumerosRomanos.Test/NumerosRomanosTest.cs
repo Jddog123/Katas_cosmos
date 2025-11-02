5eac7dcab6178f76269c1b67837fbb23817cfe3e
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-01 21:34:48 -0500

    🔴Si_NumeroEsUno_Debe_RetornarI

--- KataCosmos/NumerosRomanos.Test/NumerosRomanosTest.cs
+++ KataCosmos/NumerosRomanos.Test/NumerosRomanosTest.cs
@@ -1,7 +1,33 @@
+
+using FluentAssertions;
+
 namespace NumerosRomanos.Test
 {
     public class NumerosRomanosTest
     {
-       
+        [Fact]
+        public void Si_NumeroEsUno_Debe_RetornarI()
+        {
+            //Arrange
+            int numero = 1;
+            var numerosRomanos = new NumerosRomanos();
+            //Act
+            numerosRomanos.Convertir(numero);
+            //Assert
+            numerosRomanos.ObtenerNumeroRomano().Should().Equals("I");
+        }
+    }
+
+    public class NumerosRomanos()
+    {
+        public void Convertir(int numero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ObtenerNumeroRomano()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
