36d31686378a6204a0e1cfc09fe593687359db6d
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-02 00:38:16 -0500

    🟢Si_NumeroEsNovecientos_Debe_RetornarCM

--- KataCosmos/NumerosRomanos.Dominio/NumerosRomanosDominio.cs
+++ KataCosmos/NumerosRomanos.Dominio/NumerosRomanosDominio.cs
@@ -11,6 +11,12 @@ namespace NumerosRomanos.Dominio
         private string _numeroRomano;
         public void Convertir(int numero)
         {
+            if(numero == 900)
+            {
+                _numeroRomano = "CM";
+                return;
+            }
+
             while (numero >= 500)
             {
                 _numeroRomano += "D";
