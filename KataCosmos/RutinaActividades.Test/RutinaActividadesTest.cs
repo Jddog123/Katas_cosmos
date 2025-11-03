5f24964b7eae715f4f0f7f0612e3132169637de4
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-03 14:37:56 -0500

    🔴Si_SolicitaActividadRealizarYLaHoraActualEstaEntreSieteYSieteYcincuentaNueveAM_Debe_RetornarLeerYEstudiar

--- KataCosmos/RutinaActividades.Test/RutinaActividadesTest.cs
+++ KataCosmos/RutinaActividades.Test/RutinaActividadesTest.cs
@@ -16,6 +16,19 @@ namespace RutinaActividades.Test
             //Assert
             Actividad.Should().BeEquivalentTo("Hacer ejercicio");
         }
+
+        [Fact]
+        public void Si_SolicitaActividadRealizarYLaHoraActualEstaEntreSieteYSieteYcincuentaNueveAM_Debe_RetornarLeerYEstudiar()
+        {
+            //Arrange
+            TimeSpan horaInicial = new TimeSpan(7, 0, 0);
+            TimeSpan horaFinal = new TimeSpan(7, 59, 59);
+            RutinaActividad rutina = new RutinaPersonalizada();
+            //Act
+            string Actividad = rutina.ActidadRealizar(horaInicial, horaFinal);
+            //Assert
+            Actividad.Should().BeEquivalentTo("Leer y estudiar");
+        }
     }
 
     internal class RutinaPersonalizada : RutinaActividad
