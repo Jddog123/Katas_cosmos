b1bed782f2891ed830b964019f299e932bce3f0f
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-03 15:16:22 -0500

    🔴Si_SolicitaActividadRealizarYLaHoraActualSonLasSeisAM_Debe_RetornarHacerEjecicio

--- KataCosmos/RutinaActividades.Test/RutinaActividadesTest.cs
+++ KataCosmos/RutinaActividades.Test/RutinaActividadesTest.cs
@@ -4,6 +4,23 @@ namespace RutinaActividades.Test
 {
     public class RutinaActividadesTest
     {
-       
+        [Fact]
+        public void Si_SolicitaActividadRealizarYLaHoraActualSonLasSeisAM_Debe_RetornarHacerEjecicio()
+        {
+            //Arrange
+            RutinaPersonalizada rutina = new RutinaPersonalizada();
+            //Act
+            string Actividad = rutina.ActividadRealizar(new TimeSpan(6, 0, 0));
+            //Assert
+            Actividad.Should().BeEquivalentTo("Hacer ejercicio");
+        }
+    }
+
+    internal class RutinaPersonalizada
+    {
+        public string ActividadRealizar(TimeSpan timeSpan)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
