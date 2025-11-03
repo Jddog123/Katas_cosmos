665813fc44b91ffb75dfa3cde11aaba1ddaa52df
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-03 15:14:00 -0500

    Se reinicia Kata

--- KataCosmos/RutinaActividades.Test/RutinaActividadesTest.cs
+++ KataCosmos/RutinaActividades.Test/RutinaActividadesTest.cs
@@ -4,62 +4,6 @@ namespace RutinaActividades.Test
 {
     public class RutinaActividadesTest
     {
-        [Fact]
-        public void Si_SolicitaActividadRealizarYLaHoraActualEstaEntreSeisYSeisYcincuentaNueveAM_Debe_RetornarHacerEjecicio()
-        {
-            //Arrange
-            TimeSpan horaInicial = new TimeSpan(6, 0, 0);
-            TimeSpan horaFinal = new TimeSpan(6, 59, 59);
-            RutinaActividad rutina = new RutinaPersonalizada();
-            //Act
-            string Actividad = rutina.ActidadRealizar(horaInicial, horaFinal);
-            //Assert
-            Actividad.Should().BeEquivalentTo("Hacer ejercicio");
-        }
-
-        [Fact]
-        public void Si_SolicitaActividadRealizarYLaHoraActualEstaEntreSieteYSieteYcincuentaNueveAM_Debe_RetornarLeerYEstudiar()
-        {
-            //Arrange
-            TimeSpan horaInicial = new TimeSpan(7, 0, 0);
-            TimeSpan horaFinal = new TimeSpan(7, 59, 59);
-            RutinaActividad rutina = new RutinaPersonalizada();
-            //Act
-            string Actividad = rutina.ActidadRealizar(horaInicial, horaFinal);
-            //Assert
-            Actividad.Should().BeEquivalentTo("Leer y estudiar");
-        }
-
-        [Fact]
-        public void Si_SolicitaActividadRealizarYLaHoraActualEstaEntreOchoYOchoYcincuentaNueveAM_Debe_RetornarDesayunar()
-        {
-            //Arrange
-            TimeSpan horaInicial = new TimeSpan(8, 0, 0);
-            TimeSpan horaFinal = new TimeSpan(8, 59, 59);
-            RutinaActividad rutina = new RutinaPersonalizada();
-            //Act
-            string Actividad = rutina.ActidadRealizar(horaInicial, horaFinal);
-            //Assert
-            Actividad.Should().BeEquivalentTo("Desayunar");
-        }
-    }
-
-    internal class RutinaPersonalizada : RutinaActividad
-    {
-        public string ActidadRealizar(TimeSpan horaInicial, TimeSpan horaFinal)
-        {
-            if (horaInicial == new TimeSpan(8, 0, 0) && horaFinal == new TimeSpan(8, 59, 59))
-                return "Desayunar";
-
-            if (horaInicial == new TimeSpan(7, 0, 0) && horaFinal == new TimeSpan(7, 59, 59))
-                return "Leer y estudiar";
-
-            return "Hacer ejercicio";
-        }
-    }
-
-    interface RutinaActividad
-    {
-        string ActidadRealizar(TimeSpan horaInicial, TimeSpan horaFinal);
+       
     }
 }
